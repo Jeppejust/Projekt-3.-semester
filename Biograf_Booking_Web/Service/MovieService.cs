93cd1cc009a43469561e5ef2c05b26e071d97623
Martin Lindblad Larsen <1074164@ucn.dk>
2019-12-02 00:21:59 +0100

    Web client er begyndt på

---
+++ Biograf_Booking_Web/Service/MovieService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Biograf_Booking_Web.WebService;
+
+namespace Biograf_Booking_Web.Service
+{
+    public class MovieService
+    {
+        public List<Movie> GetMovies()
+        {
+            using (PersonServiceClient proxy = new PersonServiceClient())
+            {
+                return proxy.GetAllMovies();
+            }
+        }
+    }
+}
